d994e3b0bd35769244dba6e05df7666dfa78aac7
EdenJohnson <eden@EdenJohnsons-MacBookAir.local>
2021-02-23 20:44:03 +0800

    a simple if

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
-
+#!/bin/sh
+#==============================
+# ArchLinux-Install-Script 
+# Open Source At 
+# https://github.com/EdenJohnson2006/ArchLinux-Install-Script
+# By EdenJohnson2006
+#==============================
+echo "判定启动方式中..."
+ls /sys/firmware/efi/efivars
+if [$? -ne 0 ];then
+    echo "BIOS/CSM兼容环境，设置变量为2..."
+    BOOT_MODE=2
+else
+    echo "uEFI环境，设置变量为0..."
+    BOOT_MODE=0
+fi
