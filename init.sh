22014422477cc52ebab61f87879959a7c8ffceff
EdenJohnson <eden@EdenJohnsons-MacBookAir.local>
2021-02-23 21:13:41 +0800

    TESTING DONT USE,DISK COUNT

--- init.sh
+++ init.sh
@@ -5,12 +5,20 @@
 # https://github.com/EdenJohnson2006/ArchLinux-Install-Script
 # By EdenJohnson2006
 #==============================
-echo "判定启动方式中..."
+echo "Booting Mode Detecting"
 ls /sys/firmware/efi/efivars
 if [$? -ne 0 ];then
-    echo "BIOS/CSM兼容环境，设置变量为2..."
+    echo "BIOS/CSM Mode , Setting BOOT_MODE to 2"
     BOOT_MODE=2
 else
-    echo "uEFI环境，设置变量为0..."
+    echo "uEFI Mode , Setting BOOT_MODE to 0"
     BOOT_MODE=0
 fi
+clear
+echo "ArchLinux Install Script Version 1"
+echo "By EdenJohnson"
+DISKS=$(fdisk -l | grep -o /dev/sd | wc -l)
+if [$DISKS -eq 0];then
+    echo "WARNING!!!NO DISK DECECTED!PLEASE CHECK!"
+    exit 1
+elif 
